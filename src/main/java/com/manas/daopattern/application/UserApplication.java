8eea04572bc5caf43635d4f1c6d8d919a0ceba37
Manas Mohanty <mohanty.manas88@gmail.com>
2018-10-17 00:17:36 +0530

    Initial commit

---
+++ src/main/java/com/manas/daopattern/application/UserApplication.java
@@ -0,0 +1,52 @@
+
+package com.manas.daopattern.application;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.manas.daopattern.config.JpaEntityManagerFactory;
+import com.manas.daopattern.daos.Dao;
+import com.manas.daopattern.daos.JpaUserDao;
+import com.manas.daopattern.entities.User;
+
+public class UserApplication {
+    
+    
+    public static void main(String[] args) {
+        User user1 = getUser(9);
+        System.out.println("user by id: "+user1);
+        updateUser(user1, new String[]{"John", "john@domain.com"});
+        saveUser(new User("Monica", "monica@domain.com"));
+        deleteUser(getUser(2));
+        getAllUsers().forEach(user -> System.out.println(user.getName()));
+    }
+    
+    private static class JpaUserDaoHolder {
+        private static final JpaUserDao jpaUserDao = new JpaUserDao(new JpaEntityManagerFactory().getEntityManager());
+    }
+    
+    public static Dao getJpaUserDao() {
+        return JpaUserDaoHolder.jpaUserDao;
+    }
+    
+    public static User getUser(long id) {
+        Optional<User> user = getJpaUserDao().get(id);
+        return user.orElseGet(()-> {return new User("non-existing user", "no-email");});
+    }
+    
+    public static List<User> getAllUsers() {
+        return getJpaUserDao().getAll();
+    }
+    
+    public static void updateUser(User user, String[] params){
+        getJpaUserDao().update(user, params);
+    }
+    
+    public static void saveUser(User user) {
+        getJpaUserDao().save(user);
+    }
+    
+    public static void deleteUser(User user) {
+        getJpaUserDao().delete(user);
+    }
+}
